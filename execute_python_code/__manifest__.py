04bcf70c1fa863c2a18c0a164c4b8636953d9fce
Jaykishan Chhatani <jaykishan.chhatani@brainvire.com>
2022-11-11 11:58:26 +0530

    [ADD]Execute Python Code Module Add

---
+++ execute_python_code/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Execute Python Code",
+    "description": """
+        Allow execution of script with User Interface within Odoo
+    """,
+    "author": "OpenERP SA",
+    "version": "0.1",
+    "depends": ["base"],
+    "data": [
+        'view/python_code_view.xml',
+    ],
+    "installable": True,
+}
