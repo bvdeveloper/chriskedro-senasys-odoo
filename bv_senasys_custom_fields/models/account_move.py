f6286757ee90e3a414882eccaee290cf941f73fa
Jaykishan Chhatani <jaykishan.chhatani@brainvire.com>
2023-03-17 14:59:17 +0530

    [ADD]Added PO in sale order list, invoice form and PDF, Part# in Invoice lines.

--- bv_senasys_custom_fields/models/account_move.py
+++ bv_senasys_custom_fields/models/account_move.py
@@ -12,5 +12,5 @@ class AccountMove(models.Model):
     senasys_division = fields.Selection(related="partner_id.sale_order_ids.senasys_division")
     # inv_sent_to_cus = fields.Boolean(string="Inv Sent to Customer?")
     cust_po_submission_invoice_form = fields.Selection(related="invoice_line_ids.portal_cust_contact_method_3")
-    pull_to_invoice = fields.Char(related="invoice_line_ids.sale_line_ids.pull_to_invoice")
+    pull_to_invoice = fields.Char(related="invoice_line_ids.sale_line_ids.pull_to_invoice", string="Customer_PO#:")
     x_studio_boolean_field_YoU4G = fields.Boolean(string="Inv Sent to Customer?")
