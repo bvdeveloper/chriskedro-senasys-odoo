e58c7a9abfd0b0cbf10b16d176eef48560be9b7b
Ganesh Amale <ganeshamale51@gmail.com>
2023-11-06 10:38:19 +0530

    [UPD]Remove warning msg from the payment form

--- website_sale_checkout_skip_payment/controllers/main.py
+++ website_sale_checkout_skip_payment/controllers/main.py
@@ -3,6 +3,8 @@ from odoo.http import request
 
 from odoo.addons.payment.controllers.portal import PaymentPortal
 from odoo.addons.website_sale.controllers.main import WebsiteSale
+from odoo.addons.website_sale_delivery.controllers.main import WebsiteSaleDelivery
+
 
 
 class CheckoutSkipPaymentWebsite(WebsiteSale):
@@ -39,3 +41,12 @@ class CheckoutSkipPayment(PaymentPortal):
             )
         request.website.sale_reset()
         return request.render("website_sale.confirmation", {"order": order})
+
+class WebsiteSaleDelivery1(WebsiteSaleDelivery):
+    def _get_shop_payment_values(self, order, **kwargs):
+        res = super(WebsiteSaleDelivery, self)._get_shop_payment_values(order, **kwargs)
+        has_storable_products = any(line.product_id.type in ['consu', 'product'] for line in order.order_line)
+        if not order._get_delivery_methods() and has_storable_products:
+            res['errors'].clear()
+            res.update({'show_success_alert': True})
+        return res
