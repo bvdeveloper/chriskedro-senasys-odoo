38e1f01e4dc94f452f742075da552c3e80042fb9
Ganesh Amale <ganeshamale51@gmail.com>
2023-11-16 16:23:56 +0530

    [UPD]update salespersons list

--- bv_senasys_custom_fields/models/catlog_section.py
+++ bv_senasys_custom_fields/models/catlog_section.py
@@ -2,5 +2,16 @@ from odoo import api, fields, models, _
 
 class CatlogSection(models.Model):
     _name ='catalog.section'
-    _description = "Catlogtion Sec"
+    _description = "Catalog  Section"
     name = fields.Char(string="Catalog Section",required=True)
+
+class SalePersonList2(models.Model):
+    _name ='sales.person.two'
+    _description = "Sales Person List2"
+    name = fields.Char(string="Name",required=True)
+
+class SalePersonList3(models.Model):
+    _name ='sales.person.three'
+    _description = "Sales Person List3"
+    name = fields.Char(string="Name",required=True)
+
