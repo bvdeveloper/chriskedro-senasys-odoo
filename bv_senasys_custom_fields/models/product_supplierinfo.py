becabaf5e81e15b44dc4e49c503a7f31b154398f
riddhi_bhatiya <riddhi.bhatiya@brainvire.com>
2025-07-23 12:01:05 +0530

    [515903] Unit Cost was not pulled on to the Purchase Order and Decimal rounding into 4

--- bv_senasys_custom_fields/models/product_supplierinfo.py
+++ bv_senasys_custom_fields/models/product_supplierinfo.py
@@ -6,5 +6,8 @@ class ProductSuppierinfo(models.Model):
     _inherit = "product.supplierinfo"
 
     vendor_spec_part_notes = fields.Char(string='Vendor Spec. Part# Notes')
+    price = fields.Float(
+        'Price', default=0.0, digits='Unit Price',
+        required=True, help="The price to purchase a product")
 
 
